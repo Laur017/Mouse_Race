0ac9b1cbf01a579aec40887fcfbedf767cf994e3
Laurentiu Popa <popa.laur.01@gmail.com>
2023-12-02 14:46:48 +0200

    Generating the balls random

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -2,9 +2,48 @@ import Timer from "../Timer/Timer"
 import './Game.css'
 
 export default function Game() {
+    const redBalls = [];
+    const greenBalls = [];
+    const purpleBoxes = [];
+
+    for(let i = 0; i<10; i++){
+        const size:number = (Math.floor(Math.random() * 5) + 1)
+        redBalls.push(
+        <div className="red" style={{
+            top:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+            left:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+            width:`${size}rem`, 
+            height:`${size}rem`}}>
+        </div>)
+
+        greenBalls.push(
+            <div className="green" style={{
+                top:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+                left:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+                width:`${size}rem`, 
+                height:`${size}rem`}}>
+            </div>)
+    }
+
+    for(let i = 0; i < 5; i++){
+        const size:number = (Math.floor(Math.random() * 5) + 1)
+        purpleBoxes.push(
+            <div className="change" style={{
+                top:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+                left:`${(Math.floor(Math.random() * 10)) * 10}%`, 
+                width:`${size}rem`, 
+                height:`${size}rem`}}>
+            </div>)
+    }
+
   return (
     <div className="game-div">
         <Timer />
+        <div className="action-div">
+            {redBalls}
+            {greenBalls}
+            {purpleBoxes}    
+        </div>
     </div>
   )
 }
