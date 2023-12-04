eca7ab769134eb44affb3ee6344584f3c374fc2d
Laurentiu Popa <popa.laur.01@gmail.com>
2023-12-04 14:09:27 +0200

    Visual updates

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
+import './HomePage.css'
 import { Link } from "react-router-dom"
+import Mouse from '../../assets/mouse.png'
 
 export default function HomePage() {
   return (
-    <div>
-        <h1>Mouse Race</h1>
-        <Link to="/game">Play</Link>
+    <div className='home-page'>
+        <h1>Mouse  <img src={Mouse} />Race</h1>
+        <Link to="/game" className='play-btn'>Play</Link>
     </div>
   )
 }
